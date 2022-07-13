ce797defbcd7c17ed6b4a48fb38d65f66b4d240a
CAMIRO <juancamilosc@hotmail.com>
2022-07-13 16:29:07 -0500

    Day 3B: Associating characters to units called maps
    - Adding the overworldMap component (rendering lower and higher bg images)
    - Adding the GameLoop: a function that fires every single frame (60 times a second)

--- Overworld.js
+++ Overworld.js
@@ -3,31 +3,46 @@ class Overworld {
     this.element = config.element; // we are gonna pass in an element for the Overworld to operate on
     this.canvas = this.element.querySelector(".game-canvas"); // from the element we are gonna grab the canvas tag and save it here
     this.ctx = this.canvas.getContext("2d"); // We are going to draw into the canvas a lot, so better to save this as a reference inside the class too: this will allow us to have access to a lot of drawing methods that exist on canvas
+    this.map = null;
   }
-  init() {
-    // if you wan to draw pixel data from a image onto a canvas context,
-    // you need to load first that image into the browser in memory:
-    const image = new Image();
-    // once the browser creates it, it will copy those pixels into the canvas context
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
+  // A function that fires up every single frame (60 times a second)
+  startGameLoop() {
+    const step = () => {
+      // Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // Draw Lower layer
+      this.map.drawLowerImage(this.ctx);
+
+      // Draw Game Objects
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.x += 0.02;
+        object.sprite.draw(this.ctx);
+      });
+
+      // Draw Upper layer
+      this.map.drawUpperImage(this.ctx);
+
+      // Browser feature:
+      requestAnimationFrame(() => {
+        step();
+      });
     };
-    image.src = "/images/maps/DemoLower.png";
+    step();
+  }
 
-    //Place some Game Objects!
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    });
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: "images/characters/people/npc1.png",
-    });
+  init() {
+    // if you want to draw pixel data from a image onto a canvas context,
+    // you need to load first that image into the browser in memory:
+    // const image = new Image();
+    // once the browser creates it, it will copy those pixels into the canvas context
+    // image.onload = () => {
+    //   this.ctx.drawImage(image, 0, 0);
+    // };
+    // image.src = "/images/maps/DemoLower.png";
 
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
+    this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+
+    this.startGameLoop();
   }
 }
